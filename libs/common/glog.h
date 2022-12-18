86372baf191fd7d06f98e3bb6914a9a37129f3fc
gezhipeng0201 <gezhipeng0201@163.com>
2022-12-18 16:09:00 +0800

    init

---
+++ libs/common/glog.h
@@ -0,0 +1,9 @@
+// @author: Ge Zhipeng
+// @file: glog.h
+// @brief: glog
+#ifndef LIBS_COMMON_GLOG_H_
+#define LIBS_COMMON_GLOG_H_
+namespace toolman {
+
+}  // namespace toolman
+#endif  // LIBS_COMMON_GLOG_H_
